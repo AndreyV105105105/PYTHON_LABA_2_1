66cda61c7cc0abd8c4a10d119a5c8254fcb0b673
volva <volvachandrey108@gmail.com>
2026-03-13 20:16:48 +0300

    fix readme

--- src/contract.py
+++ src/contract.py
@@ -1,8 +1,8 @@
-from typing import Protocol, runtime_checkable, List
+from typing import Protocol, runtime_checkable
 from src.models import Task
 
 # по контракту должен возвращаться список тасков
 @runtime_checkable
 class TaskSource(Protocol):
-    def get_tasks(self) -> List[Task]:
+    def get_tasks(self) -> list[Task]:
         ...
